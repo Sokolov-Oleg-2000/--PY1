bc5d0994f20ed43389ad0a2f44cfcd3411cc3008
Sokolov-Oleg-2000 <118288366+Sokolov-Oleg-2000@users.noreply.github.com>
2022-11-25 23:47:02 +0300

    Update task6.py

--- Лабораторная работа 3/task6/task6.py
+++ Лабораторная работа 3/task6/task6.py
@@ -12,4 +12,3 @@ list_numbers[max_index], list_numbers[-1] = list_numbers[-1], list_numbers[max_i
 
 print(list_numbers)  # Ответ [2, 90, -2, 8, -36, -44, -1, -85, -14, 25, -22, -90, -100, -8, 38, -92, -45, 67, 53, 90]
 
-
