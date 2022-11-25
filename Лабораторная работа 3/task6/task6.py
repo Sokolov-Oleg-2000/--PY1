9c762c0af89287bf3c4bafc9d2adfa299f9dce43
Sokolov-Oleg-2000 <118288366+Sokolov-Oleg-2000@users.noreply.github.com>
2022-11-25 23:40:55 +0300

    Update task6.py

--- Лабораторная работа 3/task6/task6.py
+++ Лабораторная работа 3/task6/task6.py
@@ -1,9 +1,14 @@
 list_numbers = [2, 90, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-a = 0
-for i in range(0, len(list_numbers)):
-    if a <= list_numbers[i]:
-        a = list_numbers[i]
-        b = i
-list_numbers[b], list_numbers[-1] = list_numbers[-1], list_numbers[b]
+
+max_index = 0
+max_number = list_numbers[max_index]
+
+for i, current_number in enumerate(list_numbers):
+    if max_number <= current_number:
+        max_number = current_number
+        max_index = i
+
+list_numbers[max_index], list_numbers[-1] = list_numbers[-1], list_numbers[max_index]
 
 print(list_numbers)  # Ответ [2, 90, -2, 8, -36, -44, -1, -85, -14, 25, -22, -90, -100, -8, 38, -92, -45, 67, 53, 90]
+
