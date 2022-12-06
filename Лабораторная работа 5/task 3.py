f24f56fbb95158c63414157d50313b0822f8d7c3
Sokolov-Oleg-2000 <118288366+Sokolov-Oleg-2000@users.noreply.github.com>
2022-12-07 00:54:17 +0300

    Лабораторная работа 5

---
+++ Лабораторная работа 5/task 3.py
@@ -0,0 +1,13 @@
+from random import randint
+
+def get_unique_list_numbers() -> list[int]:
+    list_ = []
+    while len(list_) < 15:
+        value = randint(-10, 10)
+        if value not in list_:
+            list_.append(value)
+    return list_
+
+list_unique_numbers = get_unique_list_numbers()
+print(list_unique_numbers)
+print(len(list_unique_numbers) == len(set(list_unique_numbers)))
---
+++ Лабораторная работа 5/task 5.py
@@ -0,0 +1,8 @@
+import random
+
+def get_random_password(n=8) -> str:
+    base = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    a = random.sample(base, n)
+    return "".join(a)
+
+print(get_random_password())
