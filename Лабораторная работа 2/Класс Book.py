3165632ecfcfb6c0946f4b6ab73049fbeff8e5b3
Sokolov-Oleg-2000 <118288366+Sokolov-Oleg-2000@users.noreply.github.com>
2023-01-29 18:43:09 +0300

    Класс Book

--- Лабораторная работа 2/Класс Book.py
+++ Лабораторная работа 2/Класс Book.py
@@ -1,3 +1,4 @@
+from pydantic import BaseModel, conint
 BOOKS_DATABASE = [
     {
         "id": 1,
@@ -12,12 +13,16 @@ BOOKS_DATABASE = [
 ]
 
 
-class Book:
-    def __init__(self, id: int, name: str, pages: int):
-        self.id = id
-        self.name = name
-        self.pages = pages
-
+class Book(BaseModel):
+    id: conint(ge = 0)      # должно быть больше 0
+    name: str
+    pages: conint(ge = 0)   # должно быть больше 0
+    """
+    Создаем класс "Book", который будет инициализировать id, name, pages.
+    :param id: Идентификатор книги
+    :param name: Название книги
+    :param pages: Количество страниц в книге
+    """
     def __str__(self):
         return f'Книга "{self.name}"'
 
