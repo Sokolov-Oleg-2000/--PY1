657e88c41fc1fa84c347a136b2bdc43b0cf9b14b
Sokolov-Oleg-2000 <118288366+Sokolov-Oleg-2000@users.noreply.github.com>
2023-01-30 17:19:51 +0300

    Класс Library

--- Лабораторная работа 2/Класс Library.py
+++ Лабораторная работа 2/Класс Library.py
@@ -44,11 +44,16 @@ class Library(BaseModel):
             return len(self.books) + 1
 
     def get_index_by_book_id(self, id_ = int):
+        a = False
         for index, value in enumerate(self.books):
             if value.id == id_:
-                return index
-            else:
-                raise ValueError("Книги с запрашиваемым id не существует")
+                a = True
+                b = index
+        if a == False:
+            raise ValueError("Книги с запрашиваемым id не существует")
+        else:
+            return b
+
 
 
 if __name__ == '__main__':
@@ -62,3 +67,4 @@ if __name__ == '__main__':
     print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
 
     print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
+    
